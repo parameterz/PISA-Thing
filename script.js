27c7b43575cd44f487b411500a90a350389e0211
Dan Dyar <dan@parameterz.com>
2023-09-03 21:34:52 -0700

    chat GPT got me started here

---
+++ script.js
@@ -0,0 +1,28 @@
+// Get references to the HTML elements
+const canvas = document.getElementById("hemisphereCanvas");
+const ctx = canvas.getContext("2d");
+const radiusInput = document.getElementById("radius");
+
+function drawHemisphere() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Get the user's input for the radius
+    const radius = parseFloat(radiusInput.value);
+
+    if (isNaN(radius) || radius <= 0) {
+        alert("Please enter a valid positive number for the radius.");
+        return;
+    }
+
+    // Calculate the center of the canvas
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    // Draw the hemisphere
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, Math.PI, false);
+    ctx.fillStyle = "blue"; // You can change the color
+    ctx.fill();
+    ctx.closePath();
+}
