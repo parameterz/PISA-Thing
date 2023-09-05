6952b768d009a49ec67726b6ee508dbb35107431
Dan Dyar <dan@parameterz.com>
2023-09-04 23:12:39 -0700

    new volumetric MR calcs & page

---
+++ rf-script.js
@@ -0,0 +1,83 @@
+// Get references to the HTML elements
+// Use jQuery to select the slider elements and their corresponding value spans
+const lvotSlider = $("#lvotSlider");
+const lvotValue = $("#lvotValue");
+const vtiSlider = $('#vtiSlider');
+const vtiValue = $('#vtiValue');
+const lvedvSlider = $("#lvedvSlider");
+const lvedvValue = $("#lvedvValue");
+const efSlider = $("#efSlider");
+const efValue = $("#efValue");
+
+// Update parameters dynamically as the user adjusts the sliders
+function updateSliders() {
+    const lvot = parseFloat(lvotSlider.val());
+    const vti = parseFloat(vtiSlider.val());
+    const lvedv = parseFloat(lvedvSlider.val());
+    const ef = parseFloat(efSlider.val());
+
+    lvotValue.text(lvot);
+    lvedvValue.text(lvedv); 
+    efValue.text(ef); 
+    vtiValue.text(vti); //MR VTI
+
+    //calculate & update the Total SV
+    var totalSV = lvedv * ef/100;
+    $('.TSV').text(totalSV.toFixed(2));
+
+    //calculate & update RVol
+    var rvol = totalSV - lvot;
+    $('.rvol').text(rvol.toFixed(1));
+    //update the rvol severity
+    var rvolSeverity = ''
+    if (rvol >= 60) {
+        rvolSeverity = 'severe';
+    } else if (rvol >= 30) {
+        rvolSeverity = 'moderate';
+    } else {
+        rvolSeverity = 'mild';
+    }
+    $('.rvolSeverity').text(rvolSeverity);
+    
+    //calculate & update the RF
+    var rf = 100 * (rvol / totalSV);
+    $('.rf').text(rf.toFixed(1));
+    //update the RF severity
+    var rfSeverity = ''
+    if (rf >= 50) {
+        rfSeverity = 'severe';
+    } else if (rf >= 30){
+        rfSeverity = 'moderate';
+    } else {
+        rfSeverity = 'mild';
+    }
+    $('.rfSeverity').text(rfSeverity);
+    //calculate & update the EROA
+    var eroa = rvol / vti;
+    $('.eroa').text(eroa.toFixed(3));
+    //update the EROA severity
+    //console.log(eroa);
+    var severity = '';
+    if (eroa >= 0.40) {
+        severity = 'severe'
+    } else if (eroa >= 0.20) {
+        severity = 'moderate'; 
+    } else {
+        severity = 'mild';
+    }
+    $('.severity').text(severity);
+
+}
+
+// Attach an event handler to all sliders
+lvotSlider.on("input", updateSliders);
+lvedvSlider.on("input", updateSliders);
+efSlider.on("input", updateSliders);
+vtiSlider.on("input", updateSliders);
+
+// Initialize the sliders and make calcs
+lvotSlider.trigger("input");
+lvedvSlider.trigger("input");
+efSlider.trigger("input");
+vtiSlider.trigger("input");
+
